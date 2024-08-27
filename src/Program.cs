eee6da434405f18a6a6553b2ee257e32b3a37e6a
razan4577 <razanalmohammadi@icloud>
2024-08-27 14:04:52 +0300

    Challenge 1: String and Number Processor

--- src/Program.cs
+++ src/Program.cs
@@ -4,10 +4,16 @@ namespace FunctionChallenges
 {
     class Program
     {
+        static string StringNumberProcessor(string world, int number, int number2, string world2)
+        {
+            string sum = Convert.ToString(number + number2);
+            return world + " " + world2 + "; " + sum;
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("start from here");
-
+            Console.WriteLine(StringNumberProcessor("Hello", 100, 200, "World"));
         }
     }
 }
